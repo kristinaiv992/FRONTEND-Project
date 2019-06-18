4002db85f13c66c90d6e1a3cfb4f5acc970d0816
Kristina <kristinaiv992@gmail.com>
2019-06-18 17:13:25 +0200

    test test test

--- script.js
+++ script.js
@@ -3,19 +3,28 @@ let myData = 'https://raw.githubusercontent.com/kristinaiv992/FRONTEND-Project/m
 
 //fetch data from github
 var obj;
-
+//debugger;
+let arr =[];
 fetch('https://raw.githubusercontent.com/kristinaiv992/FRONTEND-Project/master/mydata.json')
   .then(res => res.json())
   .then(data => {
-
+console.log(data)
     obj = data;
-    let arr =[]
-    arr = obj.userUploads.foreach(user => console.log(user)
-    console.log(arr);
+    arr =[...obj.userUploads];
+    //arr = obj.userUploads.foreach(user => console.log(user));
+    // for( i=0; i<=obj.userUploads.length; i++ )
+    // {
+    //   console.log(obj[i]);
+    // }
+   // console.log(arr);
   })
-    
- 
+  .catch(err => console.log(err))
 
+  for(let i=0; i<arr.length; i++){
+    console.log(arr[i].name)
+  }
 
+console.log(arr)
+console.log(obj)
 
  
