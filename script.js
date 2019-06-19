564e9a1a16de172db93377f57235c2f81f449e73
Kristina <kristinaiv992@gmail.com>
2019-06-19 02:06:11 +0200

    added a test photo

--- script.js
+++ script.js
@@ -11,20 +11,49 @@ fetch('https://raw.githubusercontent.com/kristinaiv992/FRONTEND-Project/master/m
 console.log(data)
     obj = data;
     arr =[...obj.userUploads];
-    //arr = obj.userUploads.foreach(user => console.log(user));
-    // for( i=0; i<=obj.userUploads.length; i++ )
-    // {
-    //   console.log(obj[i]);
+userAppend(arr);
+    // for(let i=0; i<arr.length; i++){
+    //   console.log(arr[i].name)
     // }
-   // console.log(arr);
+
+  //   arr.foreach(user => console.log(user));
+  //   for( i=0; i<=obj.userUploads.length; i++ )
+  //   {
+  //     console.log(obj[i]);
+  //   }
+  //  console.log(arr);
   })
   .catch(err => console.log(err))
-
   for(let i=0; i<arr.length; i++){
-    console.log(arr[i].name)
-  }
+      console.log(arr[i].name)
+    }
 
-console.log(arr)
-console.log(obj)
 
- 
+console.log(typeof arr);
+
+let userAppend = (array) => 
+{
+  for(item of array)
+  {
+  $('body').append(
+    `
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">${item.name}</p>
+    <p class="card-text">${item.surname}</p>
+    <p class="card-text">${item.location}</p>
+    <p class="card-text">${item.type}</p>
+    <p class="card-text">${item.photo_url}</p>
+    </div>
+    </div>
+  `)
+}
+} 
+
+  
+  // <div class="card" style="width: 18rem;">
+  // <img class="card-img-top" src="..." alt="Card image cap">
+  // <div class="card-body">
+  // </div>
+  // </div>
